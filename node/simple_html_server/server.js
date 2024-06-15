157689ecda4447e030b3fefb8020de5ff4483bef
Marek Madyda <marekmadyda@gmail.com>
2024-06-15 11:30:14 +0200

    update

---
+++ node/simple_html_server/server.js
@@ -0,0 +1,20 @@
+let http = require("http");
+let url = require("url");
+let fs = require("fs");
+
+http.createServer(
+    function (req, res) {
+        const pathname = url.parse(req.url, true).pathname;
+        const filename = "./static" + pathname;
+        
+        fs.readFile(filename, function(err, data){
+            if(err) {
+                res.writeHead(404, {"Content-type" : "text/html"})
+                return res.end("404 file not found")
+            }
+
+            res.write(data);
+            res.end();
+        });
+    }
+).listen(8080);
